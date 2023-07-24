818ef49632e999b63bec9b3e749469e5607dac57
Denzel Turambi <dturambi26@gmail.com>
2023-07-24 09:47:12 -0600

    feat: Implement useState and useEffect Hooks to store data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import React from 'react';
-
+import { useState, useEffect } from 'react';
+import { getData } from '../../ApiCalls';
 
 
 function App() {
+
+  const [tricks, setTricks] = useState([])
+
+  useEffect(() => {
+    getData()
+    .then(data => setTricks(data))
+  })
+
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>
